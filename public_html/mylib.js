20b45875a2d8b14ee4224ffcc45e1f4d94eb2b44
teacheryin <teach.yin@gmail.com>
2016-03-12 01:36:19 +0800

    website

---
+++ public_html/mylib.js
@@ -0,0 +1,7 @@
+function fullScreen(theURL) {
+		window.open(theURL, '','channelmode,scrollbars');
+}
+
+function fullScreen2(theURL) {
+		window.open(theURL, '','menubar=no,scrollbars=no,status=no,toolbar=no');
+}
